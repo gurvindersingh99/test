56f54b2666592a01557f2ca144e1768f8146ac3d
gurvindersingh99 <gurvindersingh80957@gmail.com>
2025-03-09 20:28:20 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express = require("express");
+
+const app = express();
+const port = 6000;
+
+app.get("/",(req,res)=>{
+    res.send("hello world")
+})
+
+app.listen(process.env.PORT,()=>{
+    console.log("server started",port,process.env.PORT)
+})
